cf9867e3c7af42d9fc52f38ed9b98c8eec7f2fe9
Jose Hernandez <josealfredo.066@gmail.com>
2025-05-16 18:27:30 -0400

    Add event listeners for item removal and clearing the list

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.querySelector("#item-form");
 const itemInput = document.querySelector("#item-input");
 const itemList = document.querySelector("#item-list");
+const clearBtn = document.querySelector("#clear")
+
 
 function addItem(e) {
   e.preventDefault();
@@ -11,6 +13,8 @@ function addItem(e) {
     alert("Please add an item");
     return;
   }
+
+
 
   const li = document.createElement("li");
   li.innerHTML = `${itemInput.value}
@@ -23,4 +27,25 @@ function addItem(e) {
   itemInput.value = ''
 }
 
+/**
+ * @param {MouseEvent} e
+ */
+function removeItem(e) {
+  if(e.target.parentElement.classList.contains('remove-item')){
+    e.target.parentElement.parentElement.remove()
+  }
+}
+
+
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.lastChild.remove()
+  }
+}
+
+
+
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener('click', removeItem)
+clearBtn.addEventListener('click', clearItems)
