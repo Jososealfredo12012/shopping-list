5a284dbbb2eccd2adda723edea5a5859e05a961d
Jose Hernandez <josealfredo.066@gmail.com>
2025-05-16 19:16:00 -0400

    Add filtering functionality for item list

--- script.js
+++ script.js
@@ -48,6 +48,24 @@ function clearItems() {
   checkUI();
 }
 
+/**
+ * @param {InputEvent} e
+ */
+
+function filterItems(e) {
+  const items = itemList.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+  
+  items.forEach((item) =>{
+    const itemName = item.textContent.trim().toLowerCase()
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = 'flex'
+    } else{
+      item.style.display = 'none'
+    }
+  })
+}
+
 function checkUI() {
   const items = itemList.querySelectorAll("li");
   if (!items.length) {
@@ -59,9 +77,9 @@ function checkUI() {
   }
 }
 
-
-
 itemForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 clearBtn.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItems);
+
 checkUI();
