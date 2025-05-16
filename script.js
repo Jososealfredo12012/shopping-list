47d4210e20da8a6e1a2efe3182ecfc700898efec
Jose Hernandez <josealfredo.066@gmail.com>
2025-05-16 18:56:48 -0400

    Implement item removal and clear functionality with UI updates

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const itemForm = document.querySelector("#item-form");
 const itemInput = document.querySelector("#item-input");
 const itemList = document.querySelector("#item-list");
-const clearBtn = document.querySelector("#clear")
-
+const itemFilter = document.querySelector("#filter");
+const clearBtn = document.querySelector("#clear");
 
 function addItem(e) {
   e.preventDefault();
@@ -14,38 +14,54 @@ function addItem(e) {
     return;
   }
 
-
-
   const li = document.createElement("li");
   li.innerHTML = `${itemInput.value}
           <button class="remove-item btn-link text-red">
             <i class="fa-solid fa-xmark"></i>
           </button>`;
 
-  itemList.appendChild(li)
+  itemList.appendChild(li);
 
-  itemInput.value = ''
+  checkUI();
+
+  itemInput.value = "";
 }
 
 /**
  * @param {MouseEvent} e
  */
 function removeItem(e) {
-  if(e.target.parentElement.classList.contains('remove-item')){
-    e.target.parentElement.parentElement.remove()
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    e.target.parentElement.parentElement.remove();
   }
+
+  checkUI();
 }
 
-
-
 function clearItems() {
-  while (itemList.firstChild) {
-    itemList.lastChild.remove()
+  if (confirm("Are you sure?")) {
+    while (itemList.firstChild) {
+      itemList.lastChild.remove();
+    }
+  }
+
+  checkUI();
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+  if (!items.length) {
+    clearBtn.classList.add("hide");
+    itemFilter.classList.add("hide");
+  } else {
+    clearBtn.classList.remove("hide");
+    itemFilter.classList.remove("hide");
   }
 }
 
 
 
 itemForm.addEventListener("submit", addItem);
-itemList.addEventListener('click', removeItem)
-clearBtn.addEventListener('click', clearItems)
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
+checkUI();
